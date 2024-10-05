cdad4058dfc07c07e857ebec010d400515ffa494
Mohammed Moosa Sharieff <50228088+moosasharieff@users.noreply.github.com>
2024-10-05 20:22:02 +0100

    Adding Custom User Functionality to the Django Application (#2)
    I enhanced the user registration process by allowing users to create accounts using their email addresses instead of usernames. To ensure data integrity, I implemented a validation check that raises an error if the email field is left empty. Additionally, I normalized email addresses for consistency. Finally, I added a feature to create superusers, granting them elevated privileges within the system.
    
    * Feat: Created User Model and modified it to use email instead of username for authentication
    
    - Created test case and implemention for creating a user in the system.
    - Note: Make sure migrations folder with __init__.py file is present and run migrations and migrate command before running the tests.
    
    * Feat: TDD | Normalizing email input from user for authentication
    
    - Added test cases and implementation to handle normalization of different types of email shared by user; which are to be processed and stored in db
    
    * Feat: TDD | Raising ValueError exception if email is not provided by the user
    
    - Added test case and implementation for raising ValueError if user does not provide email. This helps us make our user creation more fault tolerant.
    
    * Feat: TDD | creating superuser
    
    - Added test case and implementation for creating a superuser and staff

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,52 @@
-from django.db import models # noqa
+"""
+# app/core/models.py
 
-# Create your models here.
+Models for Django Application
+"""
+
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
+
+
+class UserManager(BaseUserManager):
+    """Manager class for User Model."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """This methods create a new user in the system."""
+        if not email:
+            raise ValueError('Email was not provided!')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates a superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Appending User Model for added functionalities."""
+    email = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+
+    # Create the user
+    objects = UserManager()
+
+    REQUIRED_FIELDS = []
+
+    # Overriding in system to use 'email' instead of
+    # username when authenticating
+    USERNAME_FIELD = 'email'
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,79 @@
+"""
+# app/core/tests/test_models.py
+
+All test cases for the models (Databases) will be written here.
+"""
+
+from django.test import TestCase
+# Default User Model
+from django.contrib.auth import get_user_model
+
+
+class ModelTest(TestCase):
+    """Test case for all the models."""
+
+    def test_create_user_with_email(self):
+        """Test: Create a user successfully with email instead of username."""
+        creds = {
+            'email': 'test@example.com',
+            'password': 'testPassword123',
+        }
+
+        # Create user
+        user = get_user_model().objects.create_user(
+            email=creds['email'],
+            password=creds['password']
+        )
+
+        # Assertions
+        self.assertEqual(user.email, creds['email'])
+        self.assertTrue(user.check_password(creds['password']))
+
+    def test_user_email_normalized(self):
+        """Test if the emails provided by the user are normalized
+        and saved to the user model."""
+
+        sample_emails = [
+            ["Sample@EXAMPLE.COM", "Sample@example.com"],
+            ["SamPle@Example.COM", "SamPle@example.com"],
+            ["SamPLEe@example.COM", "SamPLEe@example.com"],
+            ["SAMPLE@EXAMPLE.com", "SAMPLE@example.com"],
+        ]
+
+        # Assertions
+        for wrong_email, correct_email in sample_emails:
+            user = get_user_model().objects.create_user(
+                email=wrong_email,
+                password='testPassword@123'
+            )
+
+            # Assertions
+            self.assertEqual(user.email, correct_email)
+
+    def test_raise_error_on_missing_email(self):
+        """Test if error is raise if email field is missing.
+
+        Note: We don't need to create a user, the assertion identifies
+        and tells us if value exception is being raised..
+        """
+
+        # Assertions
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'testPassword123')
+
+    def test_superuser_created_successfully(self):
+        """Test if we are able to create a superuser successfully."""
+        creds = {
+            'email': 'tess@example.com',
+            'password': 'testPassword@123'
+        }
+
+        # Create Superuser
+        user = get_user_model().objects.create_superuser(
+            email=creds['email'],
+            password=creds['password']
+        )
+
+        # Assertion
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
