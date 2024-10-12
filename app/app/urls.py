6d40291e9c13e31125781fef9b4a8653ff780ef0
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2024-10-12 20:03:32 +0100

    Feat: Auto API Documentation with Swagger UI
    - Created functionality to create automated API Documentation with Swagger UI using 'drf-yasg' package.
    Ref: https://medium.com/@chodvadiyasaurabh/creating-interactive-api-documentation-with-swagger-ui-in-django-53fa9e9898dc

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,6 +17,24 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Ticketing API Documentation',
+        default_version='v1',
+        description='This document explains how can the ticketing api be used by other developers.',
+        contact=openapi.Contact(email='moosa.sharieff09@gmail.com'),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/swagger-ui/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
